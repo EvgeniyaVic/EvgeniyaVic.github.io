ef1d87f6f5b0e6f93922a444127ea880d41e561c
EvgeniyaVic <evgeniyamor-92@mail.ru>
2022-04-13 18:09:30 +1000

    src

--- travel/views.py
+++ travel/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
+    name = 'Bob'
 
-    return render(request, '')
+    return render(request, 'home.html', {'name': name})
